8a2c39d412d824700eed84c03866c59bcd23cad9
Great Ogheneochuko <greatochuko123@gmail.com>
2025-03-03 13:50:28 +0100

    Refactor getSession function; enhance token verification and user retrieval from database, improving error handling and logging

--- services/authServices.ts
+++ services/authServices.ts
@@ -1,23 +1,51 @@
-import { UserType } from "@/db/models/User";
+import connectDB from "@/db/connectDB";
+import User, { UserType } from "@/db/models/User";
+import { parseJSONResponse, verifyJWT } from "@/lib/utils";
 import { cookies } from "next/headers";
 
 export async function getSession(): Promise<UserType | null> {
   try {
     const cookieStore = await cookies();
-    const token = cookieStore.get("token")?.value;
 
+    // Retrieve token from cookies
+    const token = cookieStore.get("token")?.value;
     if (!token) throw new Error("Invalid Token");
 
-    const BASE_URL = process.env.BASE_URL;
+    // Verify JWT token
+    const payload = await verifyJWT(token);
+    if (!payload || !payload.userId) {
+      throw new Error("Unable to verify token");
+    }
 
-    const res = await fetch(`${BASE_URL}/api/session`, {
-      headers: token ? { Authorization: `Bearer ${token}` } : {},
-    });
-    const { user, error } = await res.json();
-    if (error) throw new Error(error);
-    return user;
+    // Fetch user from database
+    await connectDB();
+    const user = await User.findById(payload.userId).select("-password");
+    if (!user) throw new Error("User not found");
+
+    return parseJSONResponse(user);
   } catch (error) {
     console.log("Error getting session: ", (error as Error).message);
     return null;
   }
 }
+
+// export async function getSession2(): Promise<UserType | null> {
+//   try {
+//     const cookieStore = await cookies();
+//     const token = cookieStore.get("token")?.value;
+
+//     if (!token) throw new Error("Invalid Token");
+
+//     const BASE_URL = process.env.BASE_URL;
+
+//     const res = await fetch(`${BASE_URL}/api/session`, {
+//       headers: token ? { Authorization: `Bearer ${token}` } : {},
+//     });
+//     const { user, error } = await res.json();
+//     if (error) throw new Error(error);
+//     return user;
+//   } catch (error) {
+//     console.log("Error getting session: ", (error as Error).message);
+//     return null;
+//   }
+// }
